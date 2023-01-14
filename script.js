e91f42d0ee7f5210485abe931dfb08228a2672ff
AnandReddy07 <anandillal12@gmail.com>
2023-01-14 12:16:21 +0530

    3rd commit

--- script.js
+++ script.js
@@ -74,4 +74,12 @@ submitbutton.addEventListener("click", (e) => {
     getAir(inputcityName.value);
   });
 
+<<<<<<< HEAD
+inputcityName.addEventListener("keypress",(e)=>{
+  if(e.key=="Enter"){
+    submitbutton.click()
+  }
+})
+=======
   
+>>>>>>> f9eb937f47ee5a681b66e89a14891fde382ef999
